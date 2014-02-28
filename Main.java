5299e2dfef40e46cc3065d4ebec3c3dccfc312d8
Adrian Schiller <schil227@morris.umn.edu>
2014-02-27 20:47:13 -0600

    The team made a series of methods which intakes a ball, carry it up the ramp, and throw it. We managed to make a couple of baskets, but every few times the thing that pops the ball up jams. A design flaw to be altered next time.

--- Main.java
+++ Main.java
@@ -16,49 +16,109 @@ import lejos.nxt.Sound;
 import lejos.nxt.addon.NXTMMX;
 
 public class Main {
-	 static NXTMMX multiplex = new NXTMMX(SensorPort.S4);
-	 static UltrasonicSensor Us1 = new UltrasonicSensor(SensorPort.S2);
-	 static UltrasonicSensor Us2 = new UltrasonicSensor(SensorPort.S3);
+	static NXTMMX multiplex = new NXTMMX(SensorPort.S2);
+	static UltrasonicSensor Us1 = new UltrasonicSensor(SensorPort.S4);
+	static UltrasonicSensor Us2 = new UltrasonicSensor(SensorPort.S3);
 
-	 static MMXRegulatedMotor m1 = new MMXRegulatedMotor(multiplex,
-	 NXTMMX.MMX_MOTOR_1);
-	 static MMXRegulatedMotor m2 = new MMXRegulatedMotor(multiplex,
-	 NXTMMX.MMX_MOTOR_2);
+	static MMXRegulatedMotor intake = new MMXRegulatedMotor(multiplex,
+			NXTMMX.MMX_MOTOR_1);
+	static MMXRegulatedMotor elevator = new MMXRegulatedMotor(multiplex,
+			NXTMMX.MMX_MOTOR_2);
 
 	static NXTRegulatedMotor leftWheel = Motor.B;
-	static NXTRegulatedMotor rightWheel = Motor.C;
-	static NXTRegulatedMotor thresher = Motor.A;
+	static NXTRegulatedMotor rightWheel = Motor.A;
+	static NXTRegulatedMotor thrower = Motor.C;
+	
+	static int TURN_SPEED = 200;
+	static int TURN_TIME = 1500;
 
 	public static void main(String[] args) throws InterruptedException {
-		 goForward(700);
-//		 Thread.sleep(125);
-		 Thread.sleep(125);
-		 rightWheel.stop();
-		 leftWheel.stop();
-		 thresher.stop();
-		System.out.println("stopped");
-		Thread.sleep(2700);
+//		goForward(700);
+		// Thread.sleep(125);
+//		Thread.sleep(125);
+//		rightWheel.stop();
+//		leftWheel.stop();
+//		thrower.stop();
+//		System.out.println("stopped");
+//		Thread.sleep(2700);
+//		goForward(500);
+//		Thread.sleep(2000);
+//		goBackwards(500);
+//		Thread.sleep(1000);
+//		
+//		turnLeft();
+//		Thread.sleep(1000);
+//		turnRight();
+		intake();
+		Thread.sleep(50);
+		lift();
+		Thread.sleep(100);
+		launch();
 	}
 
-	 private static void goForward(int speed) {
-	 leftWheel.setSpeed(speed);
-	 rightWheel.setSpeed(speed);
-	 leftWheel.forward();
-	 rightWheel.forward();
-	 }
+	private static void goForward(int speed) {
+		leftWheel.setSpeed(speed);
+		rightWheel.setSpeed(speed);
+		leftWheel.forward();
+		rightWheel.forward();
+	}
+
 	//
-	// private static void goBackwards(int speed) {
-	// leftWheel.setSpeed(speed);
-	// rightWheel.setSpeed(speed);
-	// leftWheel.backward();
-	// rightWheel.backward();
-	// }
-	//
-	// private static void findBall() {
-	//
-	// }
-	//
-	// private static void shootBall() {
-	//
-	// }
+	private static void goBackwards(int speed) {
+		leftWheel.setSpeed(speed);
+		rightWheel.setSpeed(speed);
+		leftWheel.backward();
+		rightWheel.backward();
+	}
+
+	private static void turnLeft() throws InterruptedException {
+		leftWheel.setSpeed(TURN_SPEED);
+		rightWheel.setSpeed(TURN_SPEED);
+		leftWheel.backward();
+		rightWheel.forward();
+		Thread.sleep(TURN_TIME);
+	}
+
+	private static void turnRight() throws InterruptedException {
+		leftWheel.setSpeed(TURN_SPEED);
+		rightWheel.setSpeed(TURN_SPEED);
+		leftWheel.forward();
+		rightWheel.backward();
+		Thread.sleep(1750);
+		}
+	
+	private static void intake() throws InterruptedException {
+		intake.setSpeed(500);
+		elevator.setSpeed(500);
+		intake.backward();
+		elevator.backward();
+		Thread.sleep(2500);
+		intake.setSpeed(0);
+		elevator.setSpeed(0);
+		intake.backward();
+		elevator.backward();
+//		intake.stop();
+//		elevator.stop();
+	}
+	
+	private static void lift() throws InterruptedException {
+		elevator.setSpeed(500);
+		thrower.setSpeed(50);
+		elevator.backward();
+		thrower.backward();
+		Thread.sleep(3500);
+		elevator.stop();
+		thrower.stop();
+	}
+	
+	private static void launch() throws InterruptedException {
+		thrower.setSpeed(700);
+		thrower.forward();
+		Thread.sleep(125);
+		thrower.stop();
+		Thread.sleep(1000);
+		thrower.backward();
+		Thread.sleep(125);
+		thrower.stop();
+	}
 }
