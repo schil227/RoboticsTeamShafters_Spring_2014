459cbf6c4d8d60c92eede9c780c59a2aa4cb21eb
Adrian Schiller <schil227@morris.umn.edu>
2014-03-30 16:34:48 -0500

    Made a lot of progress; created a goToPosition method which will make the robot drive to a specific spot in the arena, as designated by coordinates. spent a bit of time optimizing the turning methods to make it more straight, however a lot of the problem is on the processor being so bloody slow :(

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ import lejos.nxt.Motor;
 import lejos.nxt.NXTRegulatedMotor;
 import lejos.nxt.SensorPort;
 import lejos.nxt.TachoMotorPort;
+import lejos.nxt.TouchSensor;
 import lejos.nxt.UltrasonicSensor;
 import lejos.nxt.addon.ColorHTSensor;
 import lejos.nxt.addon.MMXRegulatedMotor;
@@ -18,8 +19,9 @@ import lejos.nxt.addon.NXTMMX;
 
 public class Main {
 	static NXTMMX multiplex = new NXTMMX(SensorPort.S2);
-	static UltrasonicSensor us1 = new UltrasonicSensor(SensorPort.S4);
-	static UltrasonicSensor us2 = new UltrasonicSensor(SensorPort.S3);
+	static UltrasonicSensor rightSensor = new UltrasonicSensor(SensorPort.S4);
+	static UltrasonicSensor frontSensor = new UltrasonicSensor(SensorPort.S3);
+	static TouchSensor ts = new TouchSensor(SensorPort.S1);
 
 	static MMXRegulatedMotor intake = new MMXRegulatedMotor(multiplex,
 			NXTMMX.MMX_MOTOR_1);
@@ -29,47 +31,32 @@ public class Main {
 	static NXTRegulatedMotor leftWheel = Motor.B;
 	static NXTRegulatedMotor rightWheel = Motor.A;
 	static NXTRegulatedMotor thrower = Motor.C;
-	
+
 	LCDOutputStream output = new LCDOutputStream();
-	
+
 	static int TURN_SPEED = 200;
 	static int TURN_TIME = 1500;
 
 	public static void main(String[] args) throws InterruptedException {
-//		goForward(700);
-		// Thread.sleep(125);
-//		Thread.sleep(125);
-//		rightWheel.stop();
-//		leftWheel.stop();
-//		thrower.stop();
-//		System.out.println("stopped");
-//		Thread.sleep(2700);
-//		goForward(500);
-//		Thread.sleep(2000);
-//		goBackwards(500);
-//		Thread.sleep(1000);
-//		
+		launch();
+//		leftWheel.setAcceleration(leftWheel.getAcceleration()-50);
+//		rightWheel.setAcceleration(rightWheel.getAcceleration()-50);
+		// while(!Button.ENTER.isDown()){
+		goToPosition(52, 50);
+
+	}
+
+	private static void stopWheels() throws InterruptedException {
+		leftWheel.stop();
+		rightWheel.stop();
+//		rightWheel.setSpeed(130);
+//		rightWheel.forward();
 //		Thread.sleep(500);
-//		while(!Button.ENTER.isDown()){
-//		System.out.println("dist: " + us1.getDistance());
-//		Thread.sleep(250);
-//		}
-//		turnRight();
-//		Thread.sleep(1000);
-//		turnRight();
-//		intake();
-//		Thread.sleep(50);
-//		lift();
-//		Thread.sleep(50);
-//		launch();
-		
-		turnLeft();
-		Thread.sleep(1000);
-		turnRight();
+//		rightWheel.stop();
 		
 	}
 
-	private static void goForward(int speed) {
+	private static void goBackward(int speed) {
 		leftWheel.setSpeed(speed);
 		rightWheel.setSpeed(speed);
 		leftWheel.forward();
@@ -77,57 +64,103 @@ public class Main {
 	}
 
 	//
-	private static void goBackwards(int speed) {
+	private static void goForward(int speed) {
 		leftWheel.setSpeed(speed);
 		rightWheel.setSpeed(speed);
 		leftWheel.backward();
 		rightWheel.backward();
 	}
 
-	private static void turnRight() throws InterruptedException {
+	private static void turnRight(int extraSleepTime) throws InterruptedException {
 		leftWheel.setSpeed(250);
 		rightWheel.setSpeed(250);
 		leftWheel.backward();
 		rightWheel.forward();
-		Thread.sleep(1500);
+		Thread.sleep(1500 + extraSleepTime);
 		leftWheel.stop();
 		rightWheel.stop();
 	}
 
-	private static void turnLeft() throws InterruptedException {
+	private static void turnLeft(int extraSleepTime) throws InterruptedException {
 		leftWheel.setSpeed(250);
 		rightWheel.setSpeed(250);
 		leftWheel.forward();
 		rightWheel.backward();
-		Thread.sleep(1500);
+		Thread.sleep(1500 + extraSleepTime);
 		leftWheel.stop();
 		rightWheel.stop();
-		}
+	}
+
+	private static int getFrontSensorDistance(){
+		return frontSensor.getDistance() + 10;
+	}
 	
+	private static void goToPosition(int destXCoord, int destYCoord)
+			throws InterruptedException {
+		int currentXCoord = rightSensor.getDistance();
+		int currentYCoord = getFrontSensorDistance();
+		double xDistance = destXCoord - currentXCoord;
+		double yDistance = destYCoord - currentYCoord;
+		boolean goRightX = xDistance < 0;
+		boolean goUpY = yDistance < 0;
+		int moveSpeed = 360;
+		double distanceRate = 15.0;
+		if ( Math.abs(xDistance) < 5 && Math.abs(yDistance) < 5) {
+
+		} else {
+			if (goRightX == true) {
+				turnRight(0);
+				goForward(moveSpeed);
+				Thread.sleep((long) (((Math.abs(xDistance) / distanceRate) * 1000)));
+				// Thread.sleep((long) 500);
+				stopWheels();
+				turnLeft(-100);
+			} else {
+				turnLeft(25);
+				goForward(moveSpeed);
+				Thread.sleep((long) ((Math.abs(xDistance) / distanceRate) * 1000));
+				// Thread.sleep((long) 500);
+				stopWheels();
+				turnRight(-75);
+			}
+			if (goUpY == true) {
+				goForward(moveSpeed); // 24 degrees/cm, 360 degrees for one second
+								// goes 15
+								// cm, 15cm/sec
+				Thread.sleep((long) (((Math.abs(yDistance) / distanceRate) * 1000)));
+				// Thread.sleep((long) 500);
+				stopWheels();
+			} else {
+				goBackward(moveSpeed);
+				Thread.sleep((long) ((Math.abs(yDistance) / distanceRate) * 1000));
+				// Thread.sleep((long) 500);
+				stopWheels();
+			}
+		}
+	}
+
 	private static void intake() throws InterruptedException {
 		intake.setSpeed(500);
 		elevator.setSpeed(500);
 		intake.backward();
 		elevator.backward();
-		Thread.sleep(2500);
+		Thread.sleep(4000);
 		intake.setSpeed(0);
 		elevator.setSpeed(0);
 		intake.backward();
 		elevator.backward();
-//		intake.stop();
-//		elevator.stop();
 	}
-	
+
 	private static void lift() throws InterruptedException {
 		elevator.setSpeed(500);
 		thrower.setSpeed(50);
 		elevator.backward();
 		thrower.backward();
-		Thread.sleep(3500);
+		Thread.sleep(4000);
 		elevator.stop();
 		thrower.stop();
 	}
-	
+
 	private static void launch() throws InterruptedException {
 		thrower.setSpeed(1350);
 		thrower.forward();
@@ -137,5 +170,18 @@ public class Main {
 		thrower.backward();
 		Thread.sleep(125);
 		thrower.stop();
+	}
+
+	private static void lookForBall() throws InterruptedException {
+		Thread.sleep(250);
+		while (!Button.ENTER.isDown()) {
+			if (!ts.isPressed()) {
+				goBackward(250);
+				intake();
+			} else {
+				break;
+			}
+		}
+		goBackward(0);
 	}
 }
